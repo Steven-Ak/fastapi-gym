753d8290d4b70da6b0ca0dbad3cc12a52cea52e8
Steven-Ak <stevenakram2@gmail.com>
2026-03-05 11:55:33 +0200

    fix: Import isuue

--- app/main.py
+++ app/main.py
@@ -18,7 +18,6 @@ from app.models.gym_model import Gym
 from app.models.member_model import Member 
 from app.models.member_profile_model import MemberProfile  
 from app.models.workout_plan_model import WorkoutPlan 
-from app.models.checkin_model import CheckIn 
 from app.models.qa_log_model import QALog
 from app.models.vector_document_model import VectorDocument
 
